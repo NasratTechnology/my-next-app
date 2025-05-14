f335f394004fb07ebf58afbf4dc52ab883ec9074
NasratTechnology <nasratechnologyprivatelimited@gmail.com>
2025-05-14 11:25:47 +0530

    msg

---
+++ src/app==/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import HomePage from "./Components/Home/HomePage";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <Footer />
+    </>
+  );
+}
