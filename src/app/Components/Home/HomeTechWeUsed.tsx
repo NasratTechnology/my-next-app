75fa69a44b191302affa49b1156da52ab95e8ed0
NasratTechnology <nasratechnologyprivatelimited@gmail.com>
2025-05-17 22:24:45 +0530

    responsive

--- src/app/Components/Home/HomeTechWeUsed.tsx
+++ src/app/Components/Home/HomeTechWeUsed.tsx
@@ -3,37 +3,37 @@ import Image from 'next/image';
 
 const HomeTechWeUsed = () => {
   return (
-    <section className='flex flex-wrap w-full justify-center text-center mt-12 bg-white'> 
-      <div className='container flex w-full justify-center flex-wrap'>
-        <div className='w-full text-left text-2xl'>Technologies we used</div>
+    <section className='flex flex-wrap w-full justify-center text-center pt-4 bg-white'> 
+      <div className='container flex w-full justify-center flex-wrap mt-2 md:mt-8'>
+        <div className='w-full text-left text-2xl text-black'>Technologies we used</div>
         <div className='w-full flex justify-center mt-8'>
-          <ul className='flex flex-wrap w-full justify-between '>
-            <li className='w-36 h-14 border border-gray-400'>
-                <Image src="/tech-we-used-01.jpg" alt='epaper 2' className='w-full' width={138} height={52} />
+          <ul className='flex flex-wrap w-full md:justify-between '>
+            <li className='w-26 h-14 sm:w-36 sm:h-14 border border-gray-400'>
+                <Image src="/tech-we-used-01.jpg" alt='epaper 2' className='w-[80] sm:w-[118]' width={118} height={52} />
             </li>
-            <li className='w-36 h-14 border border-gray-400'>
-                <Image src="/tech-we-used-02.jpg" alt='epaper 2' className='w-full' width={138} height={52} />
+            <li className='w-26 h-14 sm:w-36 sm:h-14 border border-gray-400'>
+                <Image src="/tech-we-used-02.jpg" alt='epaper 2' className='w-[80] sm:w-[118]' width={118} height={52} />
             </li>
-            <li className='w-36 h-14 border border-gray-400'>
-                <Image src="/tech-we-used-03.jpg" alt='epaper 2' className='w-full' width={138} height={52} />
+            <li className='w-26 h-14 sm:w-36 sm:h-14 border border-gray-400'>
+                <Image src="/tech-we-used-03.jpg" alt='epaper 2' className='w-[80] sm:w-[118]' width={118} height={52} />
             </li>
-            <li className='w-36 h-14 border border-gray-400'>
-                <Image src="/tech-we-used-04.jpg" alt='epaper 2' className='w-full' width={138} height={52} />
+            <li className='w-26 h-14 sm:w-36 sm:h-14 border border-gray-400'>
+                <Image src="/tech-we-used-04.jpg" alt='epaper 2' className='w-[80] sm:w-[118]' width={118} height={52} />
             </li>
-            <li className='w-36 h-14 border border-gray-400'>
-                <Image src="/tech-we-used-05.jpg" alt='epaper 2' className='w-full' width={138} height={52} />
+            <li className='w-26 h-14 sm:w-36 sm:h-14 border border-gray-400'>
+                <Image src="/tech-we-used-05.jpg" alt='epaper 2' className='w-[80] sm:w-[118]' width={118} height={52} />
             </li>
-            <li className='w-36 h-14 border border-gray-400'>
-                <Image src="/tech-we-used-06.jpg" alt='epaper 2' className='w-full' width={138} height={52} />
+            <li className='w-26 h-14 sm:w-36 sm:h-14 border border-gray-400'>
+                <Image src="/tech-we-used-06.jpg" alt='epaper 2' className='w-[80] sm:w-[118]' width={118} height={52} />
             </li>
-            <li className='w-36 h-14 border border-gray-400'>
-                <Image src="/tech-we-used-07.jpg" alt='epaper 2' className='w-full' width={138} height={52} />
+            <li className='w-26 h-14 sm:w-36 sm:h-14 border border-gray-400'>
+                <Image src="/tech-we-used-07.jpg" alt='epaper 2' className='w-[80] sm:w-[118]' width={118} height={52} />
             </li>
-            <li className='w-36 h-14 border border-gray-400'>
-                <Image src="/tech-we-used-08.jpg" alt='epaper 2' className='w-full' width={138} height={52} />
+            <li className='w-26 h-14 sm:w-36 sm:h-14 border border-gray-400'>
+                <Image src="/tech-we-used-08.jpg" alt='epaper 2' className='w-[80] sm:w-[118]' width={118} height={52} />
             </li>
-            <li className='w-36 h-14 border border-gray-400'>
-                <Image src="/tech-we-used-04.jpg" alt='epaper 2' className='w-full' width={138} height={52} />
+            <li className='w-26 h-14 sm:w-36 sm:h-14 border border-gray-400'>
+                <Image src="/tech-we-used-04.jpg" alt='epaper 2' className='w-[80] sm:w-[118]' width={118} height={52} />
             </li>
           </ul>
         </div>
