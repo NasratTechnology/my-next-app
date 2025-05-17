f5b6fa90cc903a9cc05ed2f3db267418ed1a4906
NasratTechnology <nasratechnologyprivatelimited@gmail.com>
2025-05-17 22:09:05 +0530

    responsive

--- src/app/Components/Home/HomeSolutionCarousel.tsx
+++ src/app/Components/Home/HomeSolutionCarousel.tsx
@@ -10,8 +10,8 @@ import "swiper/css/pagination";
 
 const HomeSolutionCarousel = () => {
   return (
-    <section className='flex flex-wrap justify-center w-full mt-8 md:mt-25 pt-16 bg-gray-100'>
-        <div className='flex flex-wrap w-full text-center'>
+    <section className='flex flex-wrap justify-center w-full mt-0 md:mt-25 pt-16 bg-gray-100'>
+        <div className='flex flex-wrap w-full text-center text-[#333]'>
           <div className='text-base w-full'>Fulfilling Your Needs for Future</div>
           <div className='text-2xl w-full'>Right Digital Needs with Right Solution</div>
         </div>        
@@ -20,17 +20,36 @@ const HomeSolutionCarousel = () => {
           <Swiper
             modules={[Navigation, Pagination, Autoplay]}
             spaceBetween={30}
-            slidesPerView={4}loop={true}             
+            slidesPerView={4}
+            loop={true}             
             // autoplay={{
             //   delay: 5000, 
             //   disableOnInteraction: true,
             // }}
             navigation
             pagination={false}
+
+            breakpoints={{
+               320: {
+                  slidesPerView: 1,
+               },
+               640: {
+                  slidesPerView: 2,
+               },
+               1024: {
+                  slidesPerView: 3,
+               },
+               1440: {
+                  slidesPerView: 4,
+               },
+            }}
+
+
+
             className="" >
             <SwiperSlide className='overflow-y-auto border border-gray-300'>
                <Image src="/HSC-01.png" alt="icon phone" className="w-full" width={300} height={380} />
-               <div className='text-center p-2.5  pb-4'>
+               <div className='text-center p-2.5 pb-4 text-[#555]'>
                   <div className='mt-1'>Google Ad Online Starter</div>
                   <div className='mt-1'>Duration : 30 days</div>
                   <div className='mt-2'>
@@ -40,7 +59,7 @@ const HomeSolutionCarousel = () => {
             </SwiperSlide>
             <SwiperSlide className='overflow-y-auto border border-gray-300'>
                 <Image src="/HSC-02.png" alt='epaper 2' className='w-full' width={300} height={380} />
-               <div className='text-center  p-2.5  pb-4'>
+               <div className='text-center p-2.5 pb-4 text-[#555]'>
                   <div className='mt-1'>Google Ad Online Starter</div>
                   <div className='mt-1'>Duration : 30 days</div>
                   <div className='mt-2'>
@@ -50,7 +69,7 @@ const HomeSolutionCarousel = () => {
             </SwiperSlide>
             <SwiperSlide className='overflow-y-auto border border-gray-300'>
                 <Image src="/HSC-03.png" alt='epaper 2' className='w-full' width={300} height={380} />
-               <div className='text-center  p-2.5  pb-4'>
+               <div className='text-center p-2.5 pb-4 text-[#555]'>
                   <div className='mt-1'>Google Ad Online Starter</div>
                   <div className='mt-1'>Duration : 30 days</div>     
                   <div className='mt-2'>
@@ -60,7 +79,7 @@ const HomeSolutionCarousel = () => {
             </SwiperSlide>
             <SwiperSlide className='overflow-y-auto border border-gray-300'>
                 <Image src="/HSC-04.png" alt='epaper 2' className='w-full' width={300} height={380} />
-               <div className='text-center  p-2.5  pb-4'>
+               <div className='text-center p-2.5 pb-4 text-[#555]'>
                   <div className='mt-1'>Google Ad Online Starter</div>
                   <div className='mt-1'>Duration : 30 days</div>    
                   <div className='mt-2'>
@@ -70,7 +89,7 @@ const HomeSolutionCarousel = () => {
             </SwiperSlide>
             <SwiperSlide className='overflow-y-auto border border-gray-300'>
                <Image src="/HSC-01.png" alt="icon phone" className="w-full" width={300} height={380} />
-               <div className='text-center  p-2.5  pb-4'>
+               <div className='text-center p-2.5 pb-4 text-[#555]'>
                   <div className='mt-1'>Google Ad Online Starter</div>
                   <div className='mt-1'>Duration : 30 days</div>    
                   <div className='mt-2'>
@@ -80,7 +99,7 @@ const HomeSolutionCarousel = () => {
             </SwiperSlide>
             <SwiperSlide className='overflow-y-auto border border-gray-300'>
                 <Image src="/HSC-02.png" alt='epaper 2' className='w-full' width={300} height={380} />
-               <div className='text-center  p-2.5  pb-4'>
+               <div className='text-center p-2.5 pb-4 text-[#555]'>
                   <div className='mt-1'>Google Ad Online Starter</div>
                   <div className='mt-1'>Duration : 30 days</div>      
                   <div className='mt-2'>
@@ -90,7 +109,7 @@ const HomeSolutionCarousel = () => {
             </SwiperSlide>
             <SwiperSlide className='overflow-y-auto border border-gray-300'>
                 <Image src="/HSC-03.png" alt='epaper 2' className='w-full' width={300} height={380} />
-               <div className='text-center  p-2.5  pb-4'>
+               <div className='text-center p-2.5 pb-4 text-[#555]'>
                   <div className='mt-1'>Google Ad Online Starter</div>
                   <div className='mt-1'>Duration : 30 days</div>    
                   <div className='mt-2'>
@@ -100,8 +119,8 @@ const HomeSolutionCarousel = () => {
             </SwiperSlide>
             <SwiperSlide className='overflow-y-auto border border-gray-300'>
                 <Image src="/HSC-04.png" alt='epaper 2' className='w-full' width={300} height={380} />
-               <div className='text-center  p-2.5  pb-4'>
-                  <div className='mt-1'>Google Ad Online Starter</div>
+               <div className='text-center p-2.5 pb-4 text-[#555]'>
+                  <div className='mt-1'>Google Ad Online Starter</div> 
                   <div className='mt-1'>Duration : 30 days</div>      
                   <div className='mt-2'>
                      <Link href='/' className='rounded-[20px] text-white uppercase text-[14px] border border-[#ccc] p-1.5 block w-28 font-semibold mx-auto bg-gradient-to-r from-[#ffaa85] to-[#ff2d8d]'>Get a Quote</Link>
